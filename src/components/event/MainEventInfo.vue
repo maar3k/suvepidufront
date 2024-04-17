3219899bb8392451122e4b28602e0e102e658963
Madli <madli.petuhhov89@gmail.com>
2024-04-17 11:50:58 +0300

    new event WIP

--- src/components/event/MainEventInfo.vue
+++ src/components/event/MainEventInfo.vue
@@ -2,21 +2,25 @@
   <div>
 
     <div class="input-group mb-3">
-      <span class="input-group-text me-3">Korraldaja ärinimi</span>
-      <input type="text" class="form-control ">
+<!--      siia tuleb business dropdown component (rolesDropdown järgi teha)-->
+<!--      selected businessId-->
     </div>
 
     <div class="input-group mb-3">
       <span class="input-group-text me-3">Pealkiri</span>
-      <input type="text" class="form-control ">
+      <input v-model="title" type="text" class="form-control ">
     </div>
 
     <div class="input-group mb-3">
       <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
+            <textarea v-model="description" class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
                       style="height: 100px"></textarea>
         <label for="floatingTextarea2">Sündmuse kirjeldus</label>
       </div>
+    </div>
+
+    <div class="input-group mb-3">
+      <input class="form-control" type="file" id="formFile">
     </div>
 
     <button @click="" type="submit" class="button-success btn btn-primary text-center text-nowrap">
@@ -28,6 +32,15 @@
 </template>
 <script>
 export default {
-  name: 'MainEventInfo'
+  name: 'MainEventInfo',
+  data() {
+    return {
+      businessId: 0,
+      title: '',
+      description: '',
+      imageData: ''
+    }
+  },
+
 }
 </script>
