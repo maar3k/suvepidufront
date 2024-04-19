f47d936abe338a6df3306626d6113f94ed8fb75b
Madli <madli.petuhhov89@gmail.com>
2024-04-19 14:58:47 +0300

    tickets WIP

---
+++ src/components/modal/TicketTypesModal.vue
@@ -0,0 +1,69 @@
+<template>
+  <Modal ref="modalRef">
+
+    <template #title>
+      <h3>Lisa piletite tüübid</h3>
+    </template>
+
+    <template #body>
+
+      <div class="container text-start">
+        <div class="row justify-content-center">
+          <div class="col">
+            <div class="mb-3">
+              <label for="ticket-type" class="form-label">Piletitüüp</label>
+              <input v-model="ticketTypeInfo.ticketTypeName" type="text" class="form-control" id="ticket-type">
+            </div>
+            <div class="mb-3">
+              <label for="ticket-price" class="form-label">Hind</label>
+              <input v-model="ticketTypeInfo.ticketTypePrice" type="number" class="form-control" id="ticket-price">
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </template>
+
+    <template #buttons>
+      <button @click="addNewTicketTypes" type="submit" class="btn btn-primary">Lisa</button>
+    </template>
+
+  </Modal>
+</template>
+
+<script>
+import Modal from "@/components/modal/Modal.vue";
+import router from "@/router";
+import {useRoute} from "vue-router";
+
+export default {
+  name: "TicketTypesModal",
+  components: {Modal},
+  data() {
+    return {
+      mainEventId: Number(useRoute().query.mainEventId),
+      ticketTypeInfo:
+        {
+          mainEventId: 0,
+          ticketTypeName: '',
+          ticketTypePrice: 0
+        }
+    }
+  },
+  methods: {
+    addNewTicketTypes() {
+      this.$http.post("/event/ticket-types", this.ticketTypeInfo, {
+            params: {
+              mainEventId: this.mainEventId
+            }
+          }
+      ).then(response => {
+        const mainEventId = response.data
+        router.push({name: 'eventTicketTypesRoute', query: {mainEventId: mainEventId}})
+      }).catch(() => {
+        router.push({name: 'errorRoute'})
+      })
+    },
+  }
+}
+</script>
--- src/views/EventTicketTypesView.vue
+++ src/views/EventTicketTypesView.vue
@@ -1,8 +1,6 @@
-
-
-
 <template>
   <div class="container text-center">
+    <!--    andmebaasist title-->
     <h1>Suvegrill 2024</h1>
     <div class="row">
       <div class="col-8">
@@ -10,7 +8,7 @@
         <table class="table">
           <thead>
           <tr>
-            <th scope="col">Tuubi nimi</th>
+            <th scope="col">Piletitüüp</th>
             <th scope="col">Hind</th>
           </tr>
           </thead>
@@ -30,43 +28,36 @@
 
           </tbody>
         </table>
-        <font-awesome-icon :icon="['fas', 'plus']" />
-
-
-
+        <font-awesome-icon @click="openTicketTypesModal" :icon="['fas', 'plus']"/>
 
       </div>
 
     </div>
-    <div class="row">
-      <div class="col-4">
 
-
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Kuupaev</label>
-            <input type="date" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Asukoht</label>
-            <input type="text" class="form-control" id="exampleInputPassword1">
-          </div>
-          <button type="submit" class="btn btn-primary">Lisa</button>
-
-
-
-      </div>
-
+    <div>
+      <TicketTypesModal ref="ticketTypesModalRef"/>
     </div>
+
   </div>
 </template>
+
 <script>
+import TicketTypesModal from "@/components/modal/TicketTypesModal.vue";
+
 export default {
-  name: "EventTicketTypesView"
+  name: "EventTicketTypesView",
+  components: {TicketTypesModal},
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    openTicketTypesModal() {
+      this.$refs.ticketTypesModalRef.$refs.modalRef.openModal()
+    },
+  }
 }
+
+
 </script>
-
-
-<style scoped>
-
-</style>
