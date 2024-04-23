6412b21d6b511f639ae76aeeb561957274b4e733
test <test@test>
2024-04-23 13:23:20 +0300

    Suvepidu Tickets View WIP

---
+++ src/components/modal/TicketModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <Modal ref="modalRef">
+    <template #body>
+      <div class="row">
+        <div class="container text-center">
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Kogu piletite arv</label>
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Piletituubid</option>
+              <option value="1">Penskarid</option>
+              <option value="2">Taiskasvanud</option>
+              <option value="3">Lapsed</option>
+            </select>
+          </div>
+          <div class="mb-3">
+            <label for="" class="form-label">Kogu piletite arv</label>
+            <input type="number" class="form-control" id="">
+          </div>
+          <button type="submit" class="btn btn-primary">Lisa</button>
+        </div>
+      </div>
+    </template>
+  </Modal>
+</template>
+<script>
+import Modal from "@/components/modal/Modal.vue";
+
+export default {
+  name: "TicketModal",
+  components: {Modal}
+}
+</script>
--- src/views/EventTicketsView.vue
+++ src/views/EventTicketsView.vue
@@ -1,16 +1,13 @@
-
-
-
 <template>
+  <TicketModal ref="ticketModalRef"/>
   <div class="container text-center">
     <h1>Suvegrill 2024 - Valga</h1>
     <div class="row">
       <div class="col-8">
-
         <table class="table">
           <thead>
           <tr>
-            <th scope="col">Piletituup</th>
+            <th scope="col">Piletitüüp</th>
             <th scope="col">Kogu piletite arv</th>
             <th scope="col">Saadaval</th>
           </tr>
@@ -21,54 +18,27 @@
             <td>100</td>
             <td>100</td>
           </tr>
-
           </tbody>
         </table>
-        <font-awesome-icon :icon="['fas', 'plus']" />
-
-
-
-
+        <font-awesome-icon @click="openTicketModal" :icon="['fas', 'plus']" style="cursor: pointer"/>
       </div>
-
-    </div>
-    <div class="row">
-      <div class="col-4">
-
-
-
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Kogu piletite arv</label>
-            <select class="form-select" aria-label="Default select example">
-              <option selected>Piletituubid</option>
-              <option value="1">Penskarid</option>
-              <option value="2">Taiskasvanud</option>
-              <option value="3">Lapsed</option>
-            </select>
-          </div>
-
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Kogu piletite arv</label>
-            <input type="number" class="form-control" id="exampleInputPassword1">
-          </div>
-          <button type="submit" class="btn btn-primary">Lisa</button>
-
-
-
-      </div>
-
     </div>
   </div>
 </template>
 <script>
 import router from "@/router";
+import TicketModal from "@/components/modal/TicketModal.vue";
 
 export default {
   name: "EventTicketTypesView",
+  components: {TicketModal},
   methods: {
     navigateToFeaturesCategories() {
       router.push({name: 'featureCategoryRoute'})
-    }
+    },
+    openTicketModal() {
+      this.$refs.ticketModalRef.$refs.modalRef.openModal()
+    },
   }
 }
 </script>
